95d15021b2666e2e052f3e475f6dc0aebce3f9d3
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-05-01 18:02:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function stringChop(str, size) {
   // your code here
+	let res=[];
+	let k=0;
+let strlength=str.length;
+	let n=Math.floor(strlength/size);
+	for(let i=0;i<n;i++){
+	let tempres="";
+		for(let j=k;j<k+size;j++){
+			tempres+=str[j];
+		}
+		res.push(tempres);
+		k+=size;
+	}
+	let tempres="";
+	while(k<strlength){
+		tempres+=str[k++];
+	}
+	
+	res.push(tempres);
+	return res;
 }
 
 // Do not change the code below
